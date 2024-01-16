7b01fccc567946e477c839bfefe5ce793e946b36
Justyna02bu <Justyna02bu@wp.pl>
2024-01-16 17:36:02 +0100

    checkin - kod pełny

--- src/main/java/org/pwr/lotnisko/dto/CheckInTo.java
+++ src/main/java/org/pwr/lotnisko/dto/CheckInTo.java
@@ -1,15 +1,19 @@
-package org.pwr.lotnisko.model;
+package org.pwr.lotnisko.dto;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.pwr.lotnisko.model.CheckInStatus;
+import org.pwr.lotnisko.model.Ticket;
 
 import java.util.Date;
+
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CheckIn {
+public class CheckInTo {
     long id;
     Date date;
     Ticket ticket;
---
+++ src/main/java/org/pwr/lotnisko/model/CheckInStatus.java
@@ -0,0 +1,8 @@
+package org.pwr.lotnisko.model;
+
+public enum CheckInStatus {
+    CHECK_IN_PENDING,
+    CHECK_IN_IN_PROGRESS,
+    CHECK_IN_IN_PROGRESS_SEET,
+    CHECK_IN_COMPLETED
+}
