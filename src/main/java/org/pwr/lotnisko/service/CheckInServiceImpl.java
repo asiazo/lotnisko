7d64a68c7929c35045327542194a010dd8750037
Justyna02bu <Justyna02bu@wp.pl>
2024-01-16 17:54:20 +0100

    checkin - kod pełny v2

--- src/main/java/org/pwr/lotnisko/service/CheckInServiceImpl.java
+++ src/main/java/org/pwr/lotnisko/service/CheckInServiceImpl.java
@@ -1,5 +1,22 @@
 package org.pwr.lotnisko.service;
 
-public class CheckInServiceImpl {
+import org.pwr.lotnisko.dto.CheckInTo;
+import org.pwr.lotnisko.repository.CheckinRepository;
+import org.pwr.lotnisko.repository.CheckinRepositorylmpl;
+import org.pwr.lotnisko.repository.FlightRepository;
+import org.pwr.lotnisko.repository.ReservationRepository;
 
+public class CheckInServiceImpl implements CheckInService{
+    private final CheckinRepository chceckinRepository;
+
+    public CheckInServiceImpl(FlightRepository flightRepository, ReservationRepository reservationRepository) {
+        this.chceckinRepository = new CheckinRepositorylmpl(flightRepository,reservationRepository);
+        };
+
+    @Override
+    public CheckInTo processWithCheckin(CheckInTo checkInTo) {
+        checkInTo = chceckinRepository.processWithCheckin(checkInTo);
+        return checkInTo;
+    }
 }
+
